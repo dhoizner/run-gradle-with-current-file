b53ec471bfa3c59cee93035358a18d68342d0df5
GitHub Action <action@github.com>
2021-06-28 15:35:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/dhoizner/rungradlewithcurrentfile/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dhoizner.rungradlewithcurrentfile.services
+
+import com.github.dhoizner.rungradlewithcurrentfile.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/dhoizner/rungradlewithcurrentfile/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dhoizner/rungradlewithcurrentfile/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dhoizner.rungradlewithcurrentfile.services
 
+import com.github.dhoizner.rungradlewithcurrentfile.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
